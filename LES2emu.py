fbceaa253b57bcd4fbe6c2f103e27a2ddf81ef37
Tomi Raatikainen <tomi.raatikainen@fmi.fi>
2018-11-28 13:18:33 +0200

    Updates to code for calculating LES inputs from new emulator variables.
     On branch master
     Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   LES2emu.py

--- LES2emu.py
+++ LES2emu.py
@@ -1,23 +1,27 @@
 # Functions extracting emulator and any other data from LES output NetCDF files,
 # and collection of functions for generating LES inputs.
 #
-#	Tomi Raatikanen 8.1.2018
+#	Tomi Raatikanen 28.11.2018
 #
 # Functions
 # =========
 # Use Python import to make these functions available, e.g. from LES2emu import GetEmuVars, get_netcdf_variable
+#
 # a) Functions for extracting data from the LES outputs
 #	GetEmuVars(fname,tstart,tend)
 #	get_netcdf_variable(fname,var_name,target_time,[end_time])
 #	extract_write_data(fname_template,specs,[name_out,nmax])
 #
 # b) Functions for generating LES inputs
-#	calc_psat_w(T)
-#	calc_sat_mixr(p,T)
-#	calc_rh(rw,T,press)
 #	calc_cloud_base(p_surf,theta,rw)
 #	calc_lwc_altitude(p_surf,theta,rw,zz)
 #	solve_rw(p_surf,theta,lwc,zz)
+#	solve_rw_lwp(p_surf,theta,lwp,pblh)
+#
+# c) Helper functions
+#	calc_psat_w(T)
+#	calc_sat_mixr(p,T)
+#	calc_rh(rw,T,press)
 #
 # Notes
 # =====
@@ -728,6 +732,138 @@ def solve_rw(p_surf,theta,lwc,zz):
 	#
 	return q_new
 
+
+def calc_lwp(p_surf,theta,pblh,rt):
+	# Calculate liquid water path (kg/m^2) when boundary layer liquid water potential temperature (theta [K]) and total
+	# water mixing ratio (rt [kg/kg]) are constants from surface (p_surf, Pa) up to boundary layer top (pblh, Pa or km).
+	# In addition to the liquid water path, function returns cloud base and top heights (m) and the maximum (or cloud top)
+	# liquid water mixing ratio (kg/kg).
+	#
+	# Constants
+	R=287.04	# Specific gas constant for dry air (R_specific=R/M), J/kg/K
+	Rm=461.5	# -||- for water
+	ep2=Rm/R-1.0 #M_air/M_water-1
+	cp=1005.0	# Specific heat for a constant pressure
+	rcp=R/cp
+	g=9.8
+	p00=1.0e+05
+	alvl = 2.5e+06 #  ! latent heat of vaporization
+	#
+	# It is assumed that a pblh value smaller than 10 is in kilometers and a value larger than that is Pa
+	if pblh<10.0:
+		z_top=pblh*1000. # from km to m (above surface)
+		p_top=0.
+	else:
+		z_top=10e3
+		p_top=p_surf-pblh # Pa (above surface)
+	#
+	# Outputs
+	lwp=0.	# Liquid water path (g/m^2)
+	zb=-999.	# Cloud base height (m)
+	zc=-999.	# Cloud top height (m)
+	clw_max=0.	# Maximum cloud liquid water
+	#
+	# a) Integrate to cloud base altitude
+	dz=1.			# 1 m resolution
+	z=0.				# The first altitude
+	press=p_surf	# Start from surface
+	RH=0
+	while press>p_top and z<=z_top:
+		# Temperature (K) 
+		tavg=theta*(press/p00)**rcp
+		#
+		# Current RH (%)
+		RH=calc_rh(rt,tavg,press)
+		if RH>100:
+			zb=z
+			break
+		#
+		# From z to z+dz
+		z+=dz
+		# Virtual temperature: T_virtual=T*(1+ep2*rl)
+		xsi=(1+ep2*rt)
+		# Pressure (Pa)
+		press-=g*dz*press/(R*tavg*xsi)
+	#
+	# No cloud or cloud water
+	if RH<=100: return lwp,zb,zc,clw_max
+	zb=z
+	#
+	# b) Integrate up to the given altitude
+	while press>p_top and z<=z_top:
+		# From z to z+dz
+		z+=dz
+		#
+		# Moist adiabatic lapse rate
+		#q_sat=calc_sat_mixr(press,tavg)
+		q_sat=calc_sat_mixr(press,tavg)
+		tavg-=g*(1+alvl*q_sat/(R*tavg))/(cp+alvl**2*q_sat/(Rm*tavg**2))*dz
+		#
+		# New pressure
+		xsi=(1+ep2*q_sat)
+		press-=g*dz*press/(R*tavg*xsi)
+		#
+		# Cloud water mixing ratio = totol - vapor
+		rc=max(0.,rt-q_sat)
+		# LWP integral
+		lwp+=rc*dz*press/(R*tavg*xsi)
+	#
+	# Cloud top height
+	zc=z
+	clw_max=rc
+	#
+	# Return LWP (kg/m^2) and boundary layer height (m)
+	return lwp,zb,zc,clw_max
+
+
+def solve_rw_lwp(p_surf,theta,lwp,pblh,debug=False):
+	# Solve boundary layer total water mixing ratio (kg/kg) from liquid water potentialtemperature (theta [K]), 
+	# liquid water path (lwp, kg/m^2) and boundary layer height (pblh, Pa or km) for an adiabatic cloud.
+	# For example, solve_rw_lwp(101780.,293.,100e-3,20000.) would return 0.00723684088331 [kg/kg].
+	#
+	# Constants
+	R=287.04	# Specific gas constant for dry air (R_specific=R/M), J/kg/K
+	cp=1005.0	# Specific heat for a constant pressure
+	rcp=R/cp
+	p00=1.0e+05
+	#
+	# LWP tolerance: 0.1 % but not more than 0.1e-3 kg/m^2 and not less than 1e-3 kg/kg
+	tol=min(max(0.001*lwp,0.1e-3),1e-3)
+	#
+	# Surface temperature (dry, i.e. no fog)
+	t_surf=theta*(p_surf/p00)**rcp
+	#
+	# The highest LWP when RH=100% at the surface (no fog)
+	rw_max= calc_sat_mixr(p_surf,t_surf)
+	lwp_max,zb,zc,clw_max=calc_lwp(p_surf,theta,pblh,rw_max)
+	# No fog cases
+	if lwp_max<lwp:
+		if debug: print ('Too high LWP (%5.1f g/m2), the maximum is %5.1f g/m2 (theta=%6.2f K, pblh=%3.0f hPa)')%(lwp*1e3, lwp_max*1e3,theta,pblh/100.)
+		return -999.
+	#
+	# The lowest LWP when RH=0% at the surface
+	rw_min=0.
+	lwp_min,zb,zc,clw_max=calc_lwp(p_surf,theta,pblh,rw_min)
+	if lwp_min>lwp:
+		if debug: print ('Too low LWP (%5.1f g/m2), the minimum is %5.1f g/m2 (theta=%6.2f K, pblh=%3.0f hPa)')%(lwp*1e3, lwp_max*1e3,theta,pblh/100.)
+		return -999.
+	#
+	k=0
+	while k<100:
+		rw_new=(rw_min+rw_max)*0.5
+		lwp_new,zb,zc,clw_max=calc_lwp(p_surf,theta,pblh,rw_new)
+		#
+		if abs(lwp-lwp_new)<tol or abs(rw_max-rw_min)<0.001e-3:
+			return rw_new
+		elif lwp<lwp_new:
+			rw_max=rw_new
+		else:
+			rw_min=rw_new
+		k+=1
+	#
+	# Failed
+	if debug: print ('Iteration failed: current LWP=%5.1f, target LWP=%5.1f')%(lwp_new*1e3,lwp*1e3)
+	return -999.
 
 
 
